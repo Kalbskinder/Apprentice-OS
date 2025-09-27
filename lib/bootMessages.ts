1fb5889f4ff7f1f3fe1719981c410667fa3a6a41
Kalbskinder <mimosc007@gmail.com>
2025-09-27 23:44:53 +0200

    Added boot-screen

---
+++ lib/bootMessages.ts
@@ -0,0 +1,17 @@
+export const messages = [
+  "Initializing system core...",
+  "Loading kernel modules...",
+  "Mounting root filesystem...",
+  "Detecting hardware components...",
+  "Configuring network interfaces...",
+  "Starting device manager (udevd)...",
+  "Checking disk integrity (fsck)...",
+  "Activating swap space...",
+  "Initializing random seed generator...",
+  "Starting system logging service...",
+  "Loading user profiles...",
+  "Launching window manager...",
+  "Establishing secure session...",
+  "Finalizing boot sequence...",
+  "System ready. Welcome!"
+]
